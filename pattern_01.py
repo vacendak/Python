884656fbce18f97c0591e69cdf7a774b7533a3a3
vacendak <49347859+vacendak@users.noreply.github.com>
2019-08-07 12:26:33 +0200

    Delete pattern_01.py

--- pattern_01.py
+++
@@ -1,6 +0,0 @@
-for i in range(0, 10):					#Number of rows
-	for j in range(0, i):				#Blank spaces before the stars
-		print(' ', end = ' ')
-	for k in range(0, 19 - 2 * i):		#Number of stars per row
-		print('*', end = ' ')
-	print('')							#New line after each row
